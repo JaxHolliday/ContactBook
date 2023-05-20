95a4912f52efd157218fed55767c0e5bb6214781
JaxHolliday <jaxholliday@gmail.com>
2023-05-20 17:56:43 -0400

    Creat Action in controller

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -9,16 +9,21 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using ContactBook.Data;
 using ContactBook.Models;
+using ContactBook.Enums;
 
 namespace ContactBook.Controllers
 {
     public class ContactsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public ContactsController(ApplicationDbContext context)
+        //injection => injecting objects so that we can benefit from them (methods and etc)
+
+        public ContactsController(ApplicationDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Contacts
@@ -54,6 +59,8 @@ namespace ContactBook.Controllers
         public IActionResult Create()
         {
             ViewData["AppUserID"] = new SelectList(_context.Users, "Id", "Id");
+            //getting list of states from ENUM's then casting it to a list
+            ViewData["StatesList"] = new SelectList(Enum.GetValues(typeof(States)).Cast<States>().ToList());
             return View();
         }
 
@@ -62,16 +69,31 @@ namespace ContactBook.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,AppUserID,FirstName,LastName,Birthdate,Address1,Address2,City,State,ZipCode,Email,PhoneNumber,Created,ImageData,ImageType")] Contact contact)
+        //bind should match the view form
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Birthdate,Address1,Address2,City,State,ZipCode,Email,PhoneNumber,ImageFile")] Contact contact)
         {
+            ModelState.Remove("AppUserId");
+
             if (ModelState.IsValid)
             {
+                //get the id of the user who is currently using page or app
+                contact.AppUserID = _userManager.GetUserId(User);
+                //getting created date in controller vs using form
+                contact.Created = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
+
+                if(contact.Birthdate != null)
+                {
+                    contact.Birthdate = DateTime.SpecifyKind(contact.Birthdate.Value, DateTimeKind.Utc);
+                }
+
                 _context.Add(contact);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AppUserID"] = new SelectList(_context.Users, "Id", "Id", contact.AppUserID);
-            return View(contact);
+
+            //ViewData["AppUserID"] = new SelectList(_context.Users, "Id", "Id", contact.AppUserID);
+            //sending us back to the index vs the page we were just at
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Contacts/Edit/5
