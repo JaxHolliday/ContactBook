7a7e33abc472af54b74c121499ef82ad8dfe9b23
JaxHolliday <jaxholliday@gmail.com>
2023-05-21 18:19:31 -0400

    Image Service Part 1

---
+++ Services/ImageService.cs
@@ -0,0 +1,22 @@
+﻿using ContactBook.Services.Interfaces;
+
+namespace ContactBook.Services
+{
+    public class ImageService : IImageService        
+    {
+        //extnesions list 
+        private readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+        //in case we dont find anything
+        private readonly string deafaultImage = "img/DefaultContactImage.png";
+
+        public string ConvertByteArrayToFile(byte[] filedata, string extension)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
