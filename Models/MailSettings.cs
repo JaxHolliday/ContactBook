4dcac949b8b8d2d11f168949171a2b7424a99295
JaxHolliday <jaxholliday@gmail.com>
2023-06-11 17:36:58 -0400

    Mail Settings model

---
+++ Models/MailSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactBook.Models
+{
+    public class MailSettings
+    {
+        //mimicing the settings in out "MailSettings" in our user secrets
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Host { get; set;}
+        public int Port { get; set; }
+        
+    }
+}
