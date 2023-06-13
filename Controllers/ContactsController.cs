85ea6bc4c8d4ed4f3c73b0d3cfa4aeb52a6c8985
JaxHolliday <jaxholliday@gmail.com>
2023-06-13 10:48:33 -0400

    Fix Delete View

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -348,10 +348,10 @@ namespace ContactBook.Controllers
             {
                 return NotFound();
             }
+            string appUserId = _userManager.GetUserId(User);
 
             var contact = await _context.Contacts
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                                        .FirstOrDefaultAsync(c => c.Id == id && c.AppUserID == appUserId);
             if (contact == null)
             {
                 return NotFound();
@@ -365,17 +365,17 @@ namespace ContactBook.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Contacts == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Contacts'  is null.");
-            }
-            var contact = await _context.Contacts.FindAsync(id);
+           
+            //making sure that we get the right user
+           string appUserId = _userManager.GetUserId(User);
+
+            var contact = await _context.Contacts.FirstOrDefaultAsync(c => c.Id == id && c.AppUserID == appUserId);
             if (contact != null)
             {
                 _context.Contacts.Remove(contact);
+                await _context.SaveChangesAsync();
             }
-
-            await _context.SaveChangesAsync();
+            
             return RedirectToAction(nameof(Index));
         }
 
